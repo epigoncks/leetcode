7d128d2c0926bbcd15fe0c8c7ea63a19e0e86d69
epigoncks <epigoncks@gmail.com>
2026-07-07 14:07:52 +0530

    Time: 0 ms (100%), Space: 9.6 MB (76.13%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+
+        int arr[26] = {0};
+
+        for(int i = 0; i<s.size(); i++){
+            arr[s[i]-'a']++;
+            arr[t[i]-'a']--;
+        }
+        for(int num : arr){
+            if(num != 0){
+                return false;
+            }
+
+        }
+        return true; 
+    }
+};
